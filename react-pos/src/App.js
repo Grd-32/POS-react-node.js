a22f142192c17ea5a7f59ece54e849c14deceb95
Grd-32 <pfdaniel77@gmail.com>
2022-07-28 15:12:40 +0300

    my pos system in react|node.js

---
+++ react-pos/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./js/components/Header";
+import Main from "./js/components/Main";
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+);
+
+export default App;
